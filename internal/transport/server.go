f0d5bafe378af7eb57a79f44ef6843943a0ec388
tltelevisor <tltelevisor@gmail.com>
2024-11-26 23:48:41 +0300

    port 8085

--- internal/transport/server.go
+++ internal/transport/server.go
@@ -66,7 +66,7 @@ func Server() {
 
 	})
 
-	log.Println("Server started on :8083")
-	log.Fatal(http.ListenAndServe(":8083", nil))
+	log.Println("Server started on :8085")
+	log.Fatal(http.ListenAndServe(":8085", nil))
 
 }
